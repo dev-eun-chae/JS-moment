7d17e371c71be524133b7ad4eaad99b939855716
eunchae <104378330+dev-eun-chae@users.noreply.github.com>
2022-09-11 22:06:35 +0900

    section 08

---
+++ js/class-08/js/weather.js
@@ -0,0 +1,26 @@
+const API_KEY = '6d3dc477bbeccfb08dbe3921c2449133';
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.getElementById('#weather span:first-child');
+      const city = document.getElementById('#weather span:last-child');
+
+      console.log(data);
+
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert('위치를 확인할 수 없습니다.');
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
